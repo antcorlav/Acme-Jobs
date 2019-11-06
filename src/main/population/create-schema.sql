d2c3fad4bd0c0b635e7941418501e3fb84cdd0e1
pepe1 <pepe1@10.100.224.107>
2019-11-06 20:51:59 +0100

    Task 010: Deployment

--- src/main/population/create-schema.sql
+++ src/main/population/create-schema.sql
@@ -1,7 +1,4 @@
 
-
-
-
     create table `administrator` (
        `id` integer not null,
         `version` integer not null,
@@ -36,203 +33,17 @@
     create table `challenge` (
        `id` integer not null,
         `version` integer not null,
-        `bronzeg` varchar(255),
-        `bronzer` varchar(255),
+        `bronze_goal` varchar(255),
+        `bronze_reward_amount` double precision,
+        `bronze_reward_currency` varchar(255),
         `description` varchar(255),
-        `goldg` varchar(255),
-        `goldr` varchar(255),
+        `gold_goal` varchar(255),
+        `gold_reward_amount` double precision,
+        `gold_reward_currency` varchar(255),
         `moment` datetime(6),
-        `silverg` varchar(255),
-        `silverr` varchar(255),
-        `title` varchar(255),
-        primary key (`id`)
-    ) engine=InnoDB;
-
-    create table `company_record` (
-       `id` integer not null,
-        `version` integer not null,
-        `ceo` varchar(255),
-        `description` varchar(255),
-        `email` varchar(255),
-        `indication` bit,
-        `name` varchar(255),
-        `phone` varchar(255),
-        `sector` varchar(255),
-        `star` integer,
-        `web_site` varchar(255),
-        primary key (`id`)
-    ) engine=InnoDB;
-
-    create table `configuration` (
-       `id` integer not null,
-        `version` integer not null,
-        `spam_threshold` double precision,
-        primary key (`id`)
-    ) engine=InnoDB;
-
-    create table `configuration_spam_words` (
-       `configuration_id` integer not null,
-        `spam_words` varchar(255)
-    ) engine=InnoDB;
-
-    create table `consumer` (
-       `id` integer not null,
-        `version` integer not null,
-        `user_account_id` integer,
-        `company` varchar(255),
-        `sector` varchar(255),
-        primary key (`id`)
-    ) engine=InnoDB;
-
-    create table `investor_record` (
-       `id` integer not null,
-        `version` integer not null,
-        `investor` varchar(255),
-        `sector` varchar(255),
-        `star` integer,
-        `statement` varchar(255),
-        primary key (`id`)
-    ) engine=InnoDB;
-
-    create table `offer` (
-       `id` integer not null,
-        `version` integer not null,
-        `deadline` datetime(6),
-        `moment` datetime(6),
-        `reward_max_amount` double precision,
-        `reward_max_currency` varchar(255),
-        `reward_min_amount` double precision,
-        `reward_min_currency` varchar(255),
-        `text` varchar(255),
-        `ticker` varchar(255),
-        `title` varchar(255),
-        primary key (`id`)
-    ) engine=InnoDB;
-
-    create table `provider` (
-       `id` integer not null,
-        `version` integer not null,
-        `user_account_id` integer,
-        `company` varchar(255),
-        `sector` varchar(255),
-        primary key (`id`)
-    ) engine=InnoDB;
-
-    create table `shout` (
-       `id` integer not null,
-        `version` integer not null,
-        `author` varchar(255),
-        `moment` datetime(6),
-        `text` varchar(255),
-        primary key (`id`)
-    ) engine=InnoDB;
-
-    create table `solicitud` (
-       `id` integer not null,
-        `version` integer not null,
-        `deadline` datetime(6),
-        `moment` datetime(6),
-        `reward_amount` double precision,
-        `reward_currency` varchar(255),
-        `text` varchar(255),
-        `ticker` varchar(255),
-        `title` varchar(255),
-        primary key (`id`)
-    ) engine=InnoDB;
-
-    create table `user_account` (
-       `id` integer not null,
-        `version` integer not null,
-        `enabled` bit not null,
-        `identity_email` varchar(255),
-        `identity_name` varchar(255),
-        `identity_surname` varchar(255),
-        `password` varchar(255),
-        `username` varchar(255),
-        primary key (`id`)
-    ) engine=InnoDB;
-
-    create table `hibernate_sequence` (
-       `next_val` bigint
-    ) engine=InnoDB;
-
-    insert into `hibernate_sequence` values ( 1 );
-
-    alter table `user_account` 
-       add constraint UK_castjbvpeeus0r8lbpehiu0e4 unique (`username`);
-
-    alter table `administrator` 
-       add constraint FK_2a5vcjo3stlfcwadosjfq49l1 
-       foreign key (`user_account_id`) 
-       references `user_account` (`id`);
-
-    alter table `anonymous` 
-       add constraint FK_6lnbc6fo3om54vugoh8icg78m 
-       foreign key (`user_account_id`) 
-       references `user_account` (`id`);
-
-    alter table `authenticated` 
-       add constraint FK_h52w0f3wjoi68b63wv9vwon57 
-       foreign key (`user_account_id`) 
-       references `user_account` (`id`);
-
-    alter table `configuration_spam_words` 
-       add constraint `FK5lk29cpqe3960a943x8x8j4yh` 
-       foreign key (`configuration_id`) 
-       references `configuration` (`id`);
-
-    alter table `consumer` 
-       add constraint FK_6cyha9f1wpj0dpbxrrjddrqed 
-       foreign key (`user_account_id`) 
-       references `user_account` (`id`);
-
-    alter table `provider` 
-       add constraint FK_b1gwnjqm6ggy9yuiqm0o4rlmd 
-       foreign key (`user_account_id`) 
-       references `user_account` (`id`);
-
-    create table `administrator` (
-       `id` integer not null,
-        `version` integer not null,
-        `user_account_id` integer,
-        primary key (`id`)
-    ) engine=InnoDB;
-
-    create table `announcement` (
-       `id` integer not null,
-        `version` integer not null,
-        `moment` datetime(6),
-        `more_info` varchar(255),
-        `text` varchar(255),
-        `title` varchar(255),
-        primary key (`id`)
-    ) engine=InnoDB;
-
-    create table `anonymous` (
-       `id` integer not null,
-        `version` integer not null,
-        `user_account_id` integer,
-        primary key (`id`)
-    ) engine=InnoDB;
-
-    create table `authenticated` (
-       `id` integer not null,
-        `version` integer not null,
-        `user_account_id` integer,
-        primary key (`id`)
-    ) engine=InnoDB;
-
-    create table `challenge` (
-       `id` integer not null,
-        `version` integer not null,
-        `bronzeg` varchar(255),
-        `bronzer` varchar(255),
-        `description` varchar(255),
-        `goldg` varchar(255),
-        `goldr` varchar(255),
-        `moment` datetime(6),
-        `silverg` varchar(255),
-        `silverr` varchar(255),
+        `silver_goal` varchar(255),
+        `silver_reward_amount` double precision,
+        `silver_reward_currency` varchar(255),
         `title` varchar(255),
         primary key (`id`)
     ) engine=InnoDB;
@@ -266,12 +77,8 @@
        `id` integer not null,
         `version` integer not null,
         `spam_threshold` double precision,
+        `spam_words` varchar(255),
         primary key (`id`)
-    ) engine=InnoDB;
-
-    create table `configuration_spam_words` (
-       `configuration_id` integer not null,
-        `spam_words` varchar(255)
     ) engine=InnoDB;
 
     create table `consumer` (
@@ -327,16 +134,7 @@
         primary key (`id`)
     ) engine=InnoDB;
 
-    create table `shout` (
-       `id` integer not null,
-        `version` integer not null,
-        `author` varchar(255),
-        `moment` datetime(6),
-        `text` varchar(255),
-        primary key (`id`)
-    ) engine=InnoDB;
-
-    create table `solicitud` (
+    create table `request` (
        `id` integer not null,
         `version` integer not null,
         `deadline` datetime(6),
@@ -346,6 +144,15 @@
         `text` varchar(255),
         `ticker` varchar(255),
         `title` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `shout` (
+       `id` integer not null,
+        `version` integer not null,
+        `author` varchar(255),
+        `moment` datetime(6),
+        `text` varchar(255),
         primary key (`id`)
     ) engine=InnoDB;
 
@@ -367,6 +174,12 @@
 
     insert into `hibernate_sequence` values ( 1 );
 
+    alter table `offer` 
+       add constraint UK_iex7e8fs0fh89yxpcnm1orjkm unique (`ticker`);
+
+    alter table `request` 
+       add constraint UK_9mxq3powq8tqctclj0fbi2nih unique (`ticker`);
+
     alter table `user_account` 
        add constraint UK_castjbvpeeus0r8lbpehiu0e4 unique (`username`);
 
@@ -385,11 +198,6 @@
        foreign key (`user_account_id`) 
        references `user_account` (`id`);
 
-    alter table `configuration_spam_words` 
-       add constraint `FK5lk29cpqe3960a943x8x8j4yh` 
-       foreign key (`configuration_id`) 
-       references `configuration` (`id`);
-
     alter table `consumer` 
        add constraint FK_6cyha9f1wpj0dpbxrrjddrqed 
        foreign key (`user_account_id`) 
@@ -399,6 +207,3 @@
        add constraint FK_b1gwnjqm6ggy9yuiqm0o4rlmd 
        foreign key (`user_account_id`) 
        references `user_account` (`id`);
-
-
-
